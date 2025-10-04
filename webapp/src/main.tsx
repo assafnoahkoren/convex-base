026899a9f8575d64d8333641a7c02576781f1665
Assaf Koren <assafnoahkoren@gmail.com>
2025-10-04 16:20:05 +0300

    .

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -4,8 +4,8 @@ import { ConvexAuthProvider } from '@convex-dev/auth/react'
 import { ConvexReactClient } from 'convex/react'
 import './index.css'
 import './i18n/config'
-import App from './App.tsx'
 import { GlobalContext } from './contexts/GlobalContext'
+import App from './App.tsx'
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string)
 
